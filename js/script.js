f41a2810e5c23d4436a366f55b2558e675c9685e
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-05-24 18:12:30 +0000

    save

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 George, Khalil All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: George, Khalil
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+window.onload = function() {
+  // this calculate volume of a sphere
+
+  const params = new URLSearchParams(document.location.search)
+  
+  // input
+  const radius = parseFloat(document.getElementById('radius-Of-Sphere').value)
+  // process
+  const volume = Math.abs(radius);
+  const volumeOfSphere = (4 / 3) * Math.PI * Math.pow(radius, 3);
+  // output
+  document.getElementById('volume').innerHTML
+    = 'Volume is: ' + (4 / 3) * Math.PI * Math.pow(radius, 3) + '.'
+}

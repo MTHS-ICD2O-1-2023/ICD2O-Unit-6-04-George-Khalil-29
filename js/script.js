4eda0ed4a4bf8879d796bcf02fc5811019905ce1
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-05-24 18:22:52 +0000

    save

--- js/script.js
+++ js/script.js
@@ -11,12 +11,16 @@ window.onload = function() {
 
   const params = new URLSearchParams(document.location.search)
   
-  // input
-  const radius = parseFloat(document.getElementById('radius-Of-Sphere').value)
-  // process
+  //input
+  const radius = params.get('radius')
+  console.log(aBase)
+
+  //process
   const volume = Math.abs(radius);
   const volumeOfSphere = (4 / 3) * Math.PI * Math.pow(radius, 3);
+  const dimensions = "<ul>/n<li>radius = " 
   // output
+  document.getElementById('dimensions').innerHTML = dimensions
   document.getElementById('volume').innerHTML
     = 'Volume is: ' + (4 / 3) * Math.PI * Math.pow(radius, 3) + '.'
 }
